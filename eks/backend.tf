32451286afaff2d9600de882974a0eea2635b7a0
Naga Mallesh <89628161+Mallesh511@users.noreply.github.com>
2024-08-23 13:10:47 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,11 +7,11 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "my-ews-baket13"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
+#   dynamodb_table = "Lock-Files"
+#   encrypt        = true
   }
 }
 
